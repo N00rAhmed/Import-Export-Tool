51d8d77f63886edcefb489749ac2762c09d83709
N00rAhmed <noor.ahmed232003@outlook.com>
2026-01-08 15:05:36 +0000

    removed mongosql files

--- src/Routers.js
+++ src/Routers.js
@@ -2,8 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 import Home from "./App";
-import Mongo from "./Mongo";
-import MongoSQL from "./MongoSQL";
 
 function Routers() {
   return (
@@ -11,8 +9,6 @@ function Routers() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/mongo" element={<Mongo />} />
-        <Route path="/mongosql" element={<MongoSQL />} />
       </Routes>
     </BrowserRouter>
   );
